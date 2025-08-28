c2aa7bc057409a2881c56b0384a0d187eb779f03
Shubham Mishra <codermishra53@gmail.com>
2025-08-28 13:22:04 +0530

    set the discount.

--- routes/mealBoxRoutes.js
+++ routes/mealBoxRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); // Use Cloudinary in production
 const { createMealBox, getMealBoxes } = require('../controllers/mealBox.controller');
+const auth = require('../middlewares/auth');
 
-router.post('/mealbox', upload.fields([{ name: 'boxImage' }, { name: 'actualImage' }]), createMealBox);
+router.post('/mealbox', auth, upload.fields([{ name: 'boxImage' }, { name: 'actualImage' }]), createMealBox);
 router.get('/mealbox', getMealBoxes);
 
 module.exports = router;
