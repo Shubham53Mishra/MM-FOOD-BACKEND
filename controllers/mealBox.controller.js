ae2a9469a09d330fc8cbb02eb9e20e0869ccb87d
Shubham Mishra <codermishra53@gmail.com>
2025-08-31 20:13:20 +0530

    set the path

--- controllers/mealBox.controller.js
+++ controllers/mealBox.controller.js
@@ -2,22 +2,23 @@ const Order = require('../models/Order');
 const Category = require('../models/Category');
 const SubCategory = require('../models/SubCategory');
 
-// Create a mealbox (fetches category/subcategory name and image by ID)
+// Create a mealbox (fetches category/subcategory name and image by ID, filters by vendor)
 exports.createMealBox = async (req, res) => {
 	try {
 		let mealBox = req.body;
-		// Populate categories with name and image
+		const vendorId = req.body.vendorId || req.user?.id;
+		// Populate categories with name and image, filter by vendor
 		if (Array.isArray(mealBox.categories)) {
 			mealBox.categories = await Promise.all(mealBox.categories.map(async (cat) => {
-				const dbCat = await Category.findById(cat._id || cat);
+				const dbCat = await Category.findOne({ _id: cat._id || cat, vendor: vendorId });
 				return dbCat ? { _id: dbCat._id, name: dbCat.name, image: dbCat.imageUrl || dbCat.image } : null;
 			}));
 			mealBox.categories = mealBox.categories.filter(Boolean);
 		}
-		// Populate subCategories with name and image
+		// Populate subCategories with name and image, filter by vendor
 		if (Array.isArray(mealBox.subCategories)) {
 			mealBox.subCategories = await Promise.all(mealBox.subCategories.map(async (sub) => {
-				const dbSub = await SubCategory.findById(sub._id || sub);
+				const dbSub = await SubCategory.findOne({ _id: sub._id || sub, vendor: vendorId });
 				return dbSub ? { _id: dbSub._id, name: dbSub.name, image: dbSub.imageUrl } : null;
 			}));
 			mealBox.subCategories = mealBox.subCategories.filter(Boolean);
