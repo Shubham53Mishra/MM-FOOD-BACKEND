195d77923b68505d405fe7b59441904b3795f140
Shubham Mishra <codermishra53@gmail.com>
2025-08-31 20:28:29 +0530

    set the error

--- routes/mealBoxRoutes.js
+++ routes/mealBoxRoutes.js
@@ -4,7 +4,18 @@ const router = express.Router();
 const { createMealBox } = require('../controllers/mealBox.controller');
 
 // POST /api/mealbox
-router.post('/mealbox', createMealBox);
+router.post('/', createMealBox);
+
+// GET /api/mealbox - get all mealboxes (orders with mealBox)
+const Order = require('../models/Order');
+router.get('/', async (req, res) => {
+	try {
+		const mealboxes = await Order.find({ mealBox: { $exists: true, $ne: null } });
+		res.json({ mealboxes });
+	} catch (err) {
+		res.status(500).json({ message: 'Error fetching mealboxes', error: err.message });
+	}
+});
 
 module.exports = router;
 
