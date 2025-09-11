9ccbacd99125d8498f28f17347be2b2ed60aea44
Shubham Mishra <codermishra53@gmail.com>
2025-09-11 14:14:34 +0530

    fix the error

--- controllers/mealBox.controller.js
+++ controllers/mealBox.controller.js
@@ -48,53 +48,53 @@ const cloudinary = require('../config/cloudinary');
 
 // Create MealBox Combo
 exports.createMealBox = async (req, res) => {
-		try {
-			const vendorId = req.user._id; // vendor token from auth middleware
-			const {
-				title,
-				description,
-				minQty,
-				price,
-				packagingDetails,
-				items
-			} = req.body;
+			try {
+				const vendorId = req.user._id; // vendor token from auth middleware
+				const {
+					title,
+					description,
+					minQty,
+					price,
+					packagingDetails,
+					items
+				} = req.body;
 
-			// Upload images if provided
-			let boxImageUrl = null;
-			let actualImageUrl = null;
-			let boxImageResult = null;
-			let actualImageResult = null;
+				// Upload images if provided
+				let boxImageUrl = null;
+				let actualImageUrl = null;
 
-			if (req.files && req.files.boxImage && req.files.boxImage[0]) {
-				boxImageResult = await cloudinary.uploader.upload(req.files.boxImage[0].path);
-				boxImageUrl = boxImageResult.secure_url;
+				// If files are missing, allow creation but warn in response
+				if (req.files && req.files.boxImage && req.files.boxImage[0]) {
+					const boxImageResult = await cloudinary.uploader.upload(req.files.boxImage[0].path);
+					boxImageUrl = boxImageResult.secure_url;
+				}
+				if (req.files && req.files.actualImage && req.files.actualImage[0]) {
+					const actualImageResult = await cloudinary.uploader.upload(req.files.actualImage[0].path);
+					actualImageUrl = actualImageResult.secure_url;
+				}
+
+				const mealBox = new MealBox({
+					title,
+					description,
+					minQty,
+					price,
+					packagingDetails,
+					items,
+					boxImage: boxImageUrl,
+					actualImage: actualImageUrl,
+					vendor: vendorId
+				});
+
+				await mealBox.save();
+				res.status(201).json({
+					success: true,
+					mealBox,
+					boxImageUrl,
+					actualImageUrl,
+					warning: (!boxImageUrl || !actualImageUrl) ? 'One or more images were not uploaded.' : undefined
+				});
+			} catch (error) {
+				res.status(500).json({ success: false, message: error.message });
 			}
-			if (req.files && req.files.actualImage && req.files.actualImage[0]) {
-				actualImageResult = await cloudinary.uploader.upload(req.files.actualImage[0].path);
-				actualImageUrl = actualImageResult.secure_url;
-			}
-
-			const mealBox = new MealBox({
-				title,
-				description,
-				minQty,
-				price,
-				packagingDetails,
-				items,
-				boxImage: boxImageUrl,
-				actualImage: actualImageUrl,
-				vendor: vendorId
-			});
-
-			await mealBox.save();
-			res.status(201).json({
-				success: true,
-				mealBox,
-				boxImageUrl,
-				actualImageUrl
-			});
-		} catch (error) {
-			res.status(500).json({ success: false, message: error.message });
-		}
-	};
+		};
  
