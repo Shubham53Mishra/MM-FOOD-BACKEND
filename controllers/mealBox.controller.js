ade0893afa611553fdc3b0d99476ebf91f9e5014
Shubham Mishra <codermishra53@gmail.com>
2025-09-12 14:47:50 +0530

    set the error

--- controllers/mealBox.controller.js
+++ controllers/mealBox.controller.js
@@ -183,6 +183,10 @@ exports.createMealBox = async (req, res) => {
 								const populatedMealBox = await MealBox.findById(mealBox._id).populate({ path: 'vendor', select: '_id name email mobile' });
 								const mealBoxObj = populatedMealBox.toObject();
 								console.log('Populated vendor:', mealBoxObj.vendor);
+								// If vendor is not populated, show vendor ObjectId
+								if (!mealBoxObj.vendor || Object.keys(mealBoxObj.vendor).length === 0) {
+									mealBoxObj.vendor = { _id: vendorId };
+								}
 								res.status(201).json({
 									success: true,
 									mealBox: mealBoxObj,
