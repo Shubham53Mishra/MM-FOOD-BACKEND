6a303a14fadc96d7bb6a79c48300d4aa3577183f
Shubham Mishra <codermishra53@gmail.com>
2025-09-12 16:31:01 +0530

    set the url

--- controllers/mealBox.controller.js
+++ controllers/mealBox.controller.js
@@ -12,6 +12,7 @@ exports.getMealBoxes = async (req, res) => {
 			return res.status(401).json({ success: false, message: 'Unauthorized: Vendor token required.' });
 		}
 		console.log('GET /api/mealbox for vendorId:', vendorId);
+		// Only show mealboxes for this vendor
 		const mealBoxes = await MealBox.find({ vendor: vendorId });
 		console.log('Mealboxes found:', mealBoxes.length);
 		res.status(200).json({ success: true, mealBoxes });
